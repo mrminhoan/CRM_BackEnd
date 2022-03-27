1355e0239d8f3f622326bcf48ef8b3f88ba779e7
mrminhoan <vannguyenhoang@webtd.net>
2022-03-27 16:33:31 +0700

    fix cors

--- src/index.server.js
+++ src/index.server.js
@@ -5,6 +5,31 @@ const mongoose = require('mongoose');
 const path = require('path')
 app.use('/public', express.static(path.join(__dirname, 'uploads')))
 
+
+
+// ************************************************************************************************
+// Add headers 
+// Fix lỗi Cors
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+  
+    // Pass to next layer of middleware
+    next();
+  });
+  // ************************************************************************************************
+
 // Config data in env
 env.config()
 // Use Json in express
